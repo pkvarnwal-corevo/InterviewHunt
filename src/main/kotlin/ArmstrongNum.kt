619a6a19b162e47aa72bbd322a39deb9043b73c9
Prinsu Kumar <pkvarnwal@gmail.com>
2023-08-18 09:27:39 +0530

    Feature : Armstrong : Armstrong class created

---
+++ src/main/kotlin/ArmstrongNum.kt
@@ -0,0 +1,28 @@
+fun checkArmstrong() {
+    var num = 153
+    var remainder: Int
+    val arr = ArrayList<Int>()
+    val originalNum = num
+
+    while (num != 0) {
+        remainder = num % 10
+        arr.add(remainder)
+        num /= 10
+    }
+    arr.reverse()
+    var temp = 0
+    arr.forEach { temp += calPow(it, arr.size) }
+    if (originalNum == temp) println("$originalNum is Armstrong number")
+    else println("$originalNum is not Armstrong number")
+}
+
+private fun calPow(i: Int, size: Int): Int {
+    var result = 1
+    for (j in 1..size) result *= i
+
+    return result
+}
+
+fun main() {
+    checkArmstrong()
+}
