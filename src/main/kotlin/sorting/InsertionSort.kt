9b2a24d1155e64582f44844f2b7194e3fe5d0e67
Prinsu Kumar <pkvarnwal@gmail.com>
2023-08-18 09:28:05 +0530

    Feature : Insertion : Created insertion class

---
+++ src/main/kotlin/sorting/InsertionSort.kt
@@ -0,0 +1,14 @@
+package sorting
+
+class InsertionSort {
+
+    fun insertionSort(arr: Array<Int>) {
+        
+    }
+}
+
+fun main() {
+    val arr = arrayOf(10, 3, 7, 2, 1)
+    val sort = InsertionSort()
+    sort.insertionSort(arr)
+}
